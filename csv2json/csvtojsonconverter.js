15545c1e145a512cd61697d7bf30262484eb8f35
shankargv87 <34228085+shankargv87@users.noreply.github.com>
2017-12-03 23:57:03 -0600

    csvtojson
    csvtojson

---
+++ csv2json/csvtojsonconverter.js
@@ -0,0 +1,22 @@
+const csvtojson = require('csvtojson')
+const fs = require('fs')
+
+const csvFilePath = 'customer-data.csv'
+const jsonFilePath  = 'customer-data.json'
+
+let jsonArray = [];
+
+csvtojson().fromFile(csvFilePath)
+.on('json', (result) => {
+    jsonArray.push(result)
+}).on('done', (error) => {
+    if (error) {
+        console.log('Error : ', error)
+        return
+    }
+
+    const jsonString = JSON.stringify(jsonArray)
+    fs.writeFileSync(jsonFilePath, jsonString, {'encoding': 'utf-8'})
+
+    console.log('Completed.')
+})
